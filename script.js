a33b74c996457937c61ee33edb108b7c2b060afa
Sophie Théréau <sophiethereau@hotmail.fr>
2024-02-19 16:13:37 +0100

    verification des inputs avec JS

---
+++ script.js
@@ -0,0 +1,38 @@
+let button = document.querySelector("#button");
+let postalCode = document.querySelector("#postalcode");
+let eMail = document.querySelector("#mail");
+let sSNumber = document.querySelector("#socialsecuritynumber");
+let passportDate = document.querySelector("#passportdate");
+let passportValidity = document.querySelector("#passportvalidity");
+let passportNumber = document.querySelector("#passportnumber");
+
+button.addEventListener("click", verifyInput);
+
+function verifyInput(event){
+    event.preventDefault();
+    if (!/^\d{5}$/.test(postalCode.value)){
+        let error = document.createElement("p");
+        error.innerText = "Code postal invalide";
+        postalCode.parentNode.appendChild(error);
+    }
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(eMail.value)){
+        error = document.createElement("p");
+        error.innerText = "E-mail invalide";
+        eMail.parentNode.appendChild(error);
+    }
+    if (!/^[1-2]{1}\d{12}$/.test(sSNumber.value)){
+        error = document.createElement("p");
+        error.innerText = "Numéro de Sécurité Sociale invalide";
+        sSNumber.parentNode.appendChild(error);
+    }
+    if (!/^[0-9]{2}[A-z]{2}[0-9]{5}$/.test(passportNumber)){
+        error = document.createElement("p");
+        error.innerText = "Numéro de passeport invalide.";
+        passportNumber.parentNode.appendChild(error); 
+    }
+    if (passportDate.value>passportValidity.value){
+        error = document.createElement("p");
+        error.innerText = "Date de validité du passeport incohérente avec la date de délivrance.";
+        passportValidity.parentNode.appendChild(error);
+    }
+}
