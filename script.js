f8ee6096a854de6edc02b095d165a9a788110df5
Sophie Théréau <sophiethereau@hotmail.fr>
2024-02-20 09:52:42 +0100

    ajout d'une fonctionnalité pour supprimer les erreurs déjà affichées

--- script.js
+++ script.js
@@ -5,34 +5,50 @@ let sSNumber = document.querySelector("#socialsecuritynumber");
 let passportDate = document.querySelector("#passportdate");
 let passportValidity = document.querySelector("#passportvalidity");
 let passportNumber = document.querySelector("#passportnumber");
+let error = "";
+
 
 button.addEventListener("click", verifyInput);
 
 function verifyInput(event){
     event.preventDefault();
+    let form = document.querySelector("form");
+
+    // Supprimer les span précédemment créés avant de revérifier chaque input
+    let errorSpans = document.querySelectorAll(".error");
+    errorSpans.forEach(span => span.parentNode.removeChild(span));
+
     if (!/^\d{5}$/.test(postalCode.value)){
-        let error = document.createElement("p");
+        error = document.createElement("span");
+        error.setAttribute("class", "error");
         error.innerText = "Code postal invalide";
         postalCode.parentNode.appendChild(error);
     }
     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(eMail.value)){
-        error = document.createElement("p");
+        error = document.createElement("span");
+        error.setAttribute("class", "error");
         error.innerText = "E-mail invalide";
         eMail.parentNode.appendChild(error);
     }
     if (!/^[1-2]{1}\d{12}$/.test(sSNumber.value)){
-        error = document.createElement("p");
+        error = document.createElement("span");
+        error.setAttribute("class", "error");
         error.innerText = "Numéro de Sécurité Sociale invalide";
         sSNumber.parentNode.appendChild(error);
     }
-    if (!/^[0-9]{2}[A-z]{2}[0-9]{5}$/.test(passportNumber)){
-        error = document.createElement("p");
+    if (!/^[0-9]{2}[A-Z]{2}[0-9]{5}$/.test(passportNumber.value)){
+        error = document.createElement("span");
+        error.setAttribute("class", "error");
         error.innerText = "Numéro de passeport invalide.";
         passportNumber.parentNode.appendChild(error); 
     }
     if (passportDate.value>passportValidity.value){
-        error = document.createElement("p");
+        error = document.createElement("span");
+        error.setAttribute("class", "error");
         error.innerText = "Date de validité du passeport incohérente avec la date de délivrance.";
         passportValidity.parentNode.appendChild(error);
+    }
+    if (error=="") {
+        form.submit();
     }
 }
